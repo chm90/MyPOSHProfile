3725f16422010777ab4afda17832dec21b2a369e
Christoffer Möckelind <christoffer.mockelind@live.se>
2015-09-25 13:02:37 +0200

    First Commit

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,18 @@
+﻿
+# add vim
+set-alias vim "$(${env:ProgramFiles(x86)})\vim\vim74\vim.exe"
+
+# Load posh-git example profile
+. 'C:\Users\Christoffer\Source\posh-git\profile.example.ps1'
+
+# add 7zip
+set-alias 7zip "$(${env:ProgramFiles(x86)})\7-Zip\7z.exe"
+
+# add vim config
+$VimConfig = "$($env:LOCALAPPDATA)\VirtualStore\Program Files (x86)\vim\_vimrc"
+
+# add cygwin
+set-alias cygwin "C:\cygwin64\Cygwin.bat"
+
+# add ssh
+set-alias ssh "C:\cygwin64\bin\ssh.exe"
